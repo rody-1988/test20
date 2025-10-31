5ca23566445ebb633075a0758b0b18854cf5c327
rody-1988 <xl.pogigori.808@gmail.com>
2025-10-31 19:56:16 +0900

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//ページ内スクロール
+$('a[href^="#"]').on("click", function () {
+  var speed = 300;
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? "html" : href);
+  var position = target.offset().top;
+  $("html, body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing"
+  );
+  return false;
+});
+
+//ページトップへ戻る
+var $pageTop = $(".page__top");
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+    $pageTop.fadeIn();
+  } else {
+    $pageTop.fadeOut();
+  }
+});
+$pageTop.on("click", function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    300
+  );
+  return false;
+});
